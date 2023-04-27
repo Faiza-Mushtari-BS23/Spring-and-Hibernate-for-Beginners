cf32c2469a9e5bbcdf3763535bd2ef34857bd5f7
Faiza Mushtari <128612383+BS1328@users.noreply.github.com>
2023-04-27 12:45:21 +0600

    Update customers src files

--- web-customer-tracker/src/main/java/springdemo/dao/CustomerDAOImpl.java
+++ web-customer-tracker/src/main/java/springdemo/dao/CustomerDAOImpl.java
@@ -24,9 +24,10 @@ public class CustomerDAOImpl implements CustomerDAO {
 		// get the current hibernate session
 		Session currentSession = sessionFactory.getCurrentSession();
 				
-		// create a query
+		// create a query  ... sort by last name
 		Query<Customer> theQuery = 
-				currentSession.createQuery("from Customer order by lastName", Customer.class);
+				currentSession.createQuery("from Customer order by lastName",
+											Customer.class);
 		
 		// execute query and get result list
 		List<Customer> customers = theQuery.getResultList();
@@ -35,16 +36,27 @@ public class CustomerDAOImpl implements CustomerDAO {
 		return customers;
 	}
 
-
 	@Override
 	public void saveCustomer(Customer theCustomer) {
 
 		// get current hibernate session
 		Session currentSession = sessionFactory.getCurrentSession();
 		
-		// save the customer ... finally LOL
-		currentSession.save(theCustomer);
+		// save/update the customer ... finally LOL
+		currentSession.saveOrUpdate(theCustomer);
 		
+	}
+
+	@Override
+	public Customer getCustomer(int theId) {
+
+		// get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// now retrieve/read from database using the primary key
+		Customer theCustomer = currentSession.get(Customer.class, theId);
+		
+		return theCustomer;
 	}
 
 }
